2409bbceb195d4502c82c482d00af7f201301afb
Tumee0620 <ttumee0620@gmail.com>
2025-02-25 09:30:34 +0800

    rounded circle and logo edited

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,24 @@
+import { Radius } from "@/components/Radius";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex w-full h-[100vh] ">
-      <div className="flex bg-white w-full h-full">hello</div>
-      <div className="flex bg-black w-full h-full">hello</div>
+    <div className="flex w-full h-[100vh] relative ">
+      <div>
+        <Radius />
+        <Radius className={"w-[340px] h-[340px]"} />
+        <Radius className={"w-[540px] h-[540px]"} />
+        <Radius className={"w-[940px] h-[940px]"} />
+        <Radius className={"w-[1340px] h-[1340px]"} />
+      </div>
+      <div className="w-[139px] h-[139px] rounded-full absolute left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] bg-white"></div>
+      <div className="flex absolute gap-4 left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%]">
+        <img className="w-[43.289px] h-[86px]" src="/gg.svg"></img>
+        <img className="w-[43.289px] h-[86px]" src="/Vector.svg"></img>
+      </div>
+
+      <div className="flex bg-white w-full h-full"></div>
+      <div className="flex bg-[#0F141E] w-full h-full"></div>
     </div>
   );
 }
---
+++ src/components/Radius.jsx
@@ -0,0 +1,7 @@
+export const Radius = ({ className }) => {
+  return (
+    <div
+      className={`flex w-[140px] h-[140px] border-[1px] border-solid rounded-full absolute left-[50%] translate-x-[-50%] translate-y-[-50%] top-[50%] ${className}`}
+    ></div>
+  );
+};
